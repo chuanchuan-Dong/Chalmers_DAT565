e3fc641749c4b313aac94e7fdcc18d04055e7a0d
Wang Ziyuan <756881257@qq.com>
2023-08-29 20:51:09 +0200

    task1 finished

---
+++ assignment1/README.md
@@ -0,0 +1,11 @@
+Tasks:
+1. Which countries have a life expectancy higher than one standard deviation above the mean?
+
+2. Which countries have high life expectancy but have low GDP (note the difference between GDP
+and GDP per capita)? Motivate how you have chosen to define “high” and “low.”
+
+3. Does every strong economy (normally indicated by GDP) have high life expectancy?
+
+4. Related to the above question (question 3), what happens if you use GDP per capita as an
+indicator of a strong economy as opposed to GDP alone? Explain the results you obtain through
+this analysis, and discuss any insights you get from comparing these results to question 3.
--- assignment1/code/assignment1.py
+++ assignment1/code/assignment1.py
@@ -1,20 +1,76 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-root = 'python/DAT565/data/'
-data = pd.read_csv(root + 'gdp-per-capita-penn-world-table.csv')
+root = './assignment1/data/'
 
-# print(data.shape)
-# print(data.head())
+def preprocess_data():
+    gdp_data = pd.read_csv(root + 'gdp-per-capita-penn-world-table.csv')
+    life_data = pd.read_csv(root + 'life-expectancy-at-birth-total-years.csv')
+    life_columns, gdb_columns = life_data.columns, gdp_data.columns
+    year = life_columns[2]
+    life_country, gdb_country = set(life_data[life_columns[0]]), set(gdp_data[gdb_columns[0]])
+    
+    # find the same countries
+    country = list(life_country & gdb_country)
+    # print(country)
+    
+    new_life_data = life_data[(life_data[year] >= 1970) & (life_data[year] <= 2019)]
+    new_life_data = new_life_data[new_life_data[life_columns[0]].isin(country)]
+    new_gdp_data = gdp_data[gdp_data[gdb_columns[0]].isin(country)]
+    
+    # d = dict()
+    # diff = 0
+    # for item in country:
+    #     value = [len(new_life_data[new_life_data[life_columns[0]] == item]), len(new_gdp_data[new_gdp_data[gdb_columns[0]] == item])]
+    #     d[item] = value
+    #     diff += value[1] - value[0]
+    # # print(new_life_data.head)
+    # print(d)
+    # print(diff)    
+    
+    print(len(new_life_data))
+    print(len(new_gdp_data))
 
-# check the country data
-columns = data.columns
-# entity = set(data[columns[0]])
+    return new_gdp_data, new_life_data
+
+def task1():
+    gdp_data, life_data = preprocess_data()
+    mean_life, std_life = life_data[life_data.columns[3]].mean(), life_data[life_data.columns[3]].std()
+    result = life_data[[life_data.columns[0], life_data.columns[3]]].groupby('Entity').mean()
+    result = result.sort_values(by=life_data.columns[3], ascending=False)
+    result = result[result[result.columns[0]] >= mean_life + std_life]
+    print(mean_life)    
+    print(std_life)
+    print(result)
+    print(type(result))
+
+task1()
+
+
+# def check_life_mean():
+#     columns = life_data.columns
+#     life_item = columns[3]
+#     life_mean = life_data[life_item].mean()
+#     life_var = life_mean
+#     print(life_mean)
+#     print(columns)
+
+# check_life_mean()
+
+# print(life_data.head())
+exit(0)
+
+# print(gdb_data.shape)
+# print(gdb_data.head())
+
+# check the country gdb_data
+columns = gdp_data.columns
+# entity = set(gdb_data[columns[0]])
 # print(entity)
 # print(len(entity))
 
-# print(data[columns[0] == 'Albania'])
+# print(gdb_data[columns[0] == 'Albania'])
 
-albania_data = data[data[columns[0]] == 'Albania']
-plot_data = albania_data
-print(albania_data)
+albania_gdb_data = gdp_data[gdp_data[columns[0]] == 'Albania']
+plot_gdb_data = albania_gdb_data
+print(albania_gdb_data)
