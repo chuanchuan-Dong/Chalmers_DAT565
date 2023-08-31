f83515327c4dc4bb092bde24569f8bcda998a7d9
chuanchuan <dongyuchuan001@outlook.com>
2023-08-31 13:07:03 +0200

    add GdpVSLifeExp

--- assignment1/code/assignment1.py
+++ assignment1/code/assignment1.py
@@ -40,7 +40,6 @@ CombinedData = pd.merge(CombinedData, LifeExpData, on=['Entity','Year', 'Code'],
 CombinedData = pd.merge(CombinedData, GdpData,on = ['Entity', 'Year', 'Code'], how='left' )
 CombinedData = pd.merge(CombinedData, NationalGdpData, how='left' )
 
-##########HIIIIIIIII
 
 #Fill the miss data by the mean of each country
 CountryMeans = CombinedData.groupby('Entity')[[LifeExpShort,GdpShort, NationalGdpShort]].mean().reset_index().values
@@ -51,7 +50,23 @@ CombinedData[LifeExpShort] = CombinedData[LifeExpShort].fillna(CombinedData['Ent
 CombinedData[GdpShort] = CombinedData[GdpShort].fillna(CombinedData['Entity'].apply(lambda x: IndexDict.get(x)[1]))
 CombinedData[NationalGdpShort] = CombinedData[NationalGdpShort].fillna(CombinedData['Entity'].apply(lambda x: IndexDict.get(x)[2]))
 CombinedData.to_csv('./data/test.csv')
-exit(0)
+
+'''
+Following task
+'''
+def GdpVSExp():
+    for country in all_CountryList:
+        plt.scatter(CombinedData[CombinedData['Entity']==country][GdpShort],
+                    CombinedData[CombinedData['Entity']==country][LifeExpShort], 
+                    s=5,
+                    label=country)
+    plt.title('Scatter Plot of GDP per Capita vs. Life Expectancy')
+    plt.xlabel('GDP per Capita')
+    plt.ylabel('Life Expectancy')
+    plt.legend(title='Country')
+    plt.grid(True)
+    plt.show()   
+
 
 def task1(DataSet):
     LifeExp_Mean, LifeExp_std = DataSet[LifeExpShort].mean(), DataSet[LifeExpShort].std()
@@ -98,6 +113,8 @@ def task3(Dataset:pd.DataFrame, threshold = 0.1):
 
 if __name__ == '__main__':
 # Uncomment when executing task
+  GdpVSExp()
+
   # task1(CombinedData)
   # task2(CombinedData)
-  task3(CombinedData)
+  #task3(CombinedData)
