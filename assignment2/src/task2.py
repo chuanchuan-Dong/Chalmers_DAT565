c866b13eeb196e0125bd1853b041aafb8f3073c9
cici <linmeixi@linmeixideMacBook-Air.local>
2023-09-08 12:20:00 +0200

    knn

--- assignment2/src/task2.py
+++ assignment2/src/task2.py
@@ -2,10 +2,61 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from matplotlib.colors import ListedColormap
+from sklearn import datasets, neighbors
+from sklearn.inspection import DecisionBoundaryDisplay
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import some data to play with
+iris = datasets.load_iris()
+print(iris)
+exit(0)
+# we only take the first two features. We could avoid this ugly
+# slicing by using a two-dim dataset
+X = iris.data[:, :2]
+y = iris.target
+
+# Create color maps
+cmap_light = ListedColormap(["white", "cyan", "cornflowerblue"])
+cmap_bold = ["blue", "c", "darkblue"]
 
 
+for weights in ["uniform", "distance"]:
+    for i, n_neighbors in enumerate(range(1,90,10)):
+        # we create an instance of Neighbours Classifier and fit the data.
+        clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+        clf.fit(X, y)
+
+        ax = plt.subplot(3,3,i+1)
+        DecisionBoundaryDisplay.from_estimator(
+            clf,
+            X,
+            cmap=cmap_light,
+            ax=ax,
+            response_method="predict",
+            plot_method="pcolormesh",
+            xlabel=iris.feature_names[0],
+            ylabel=iris.feature_names[1],
+            shading="auto",
+        )
+
+        # Plot also the training points
+        sns.scatterplot(
+            x=X[:, 0],
+            y=X[:, 1],
+            hue=iris.target_names[y],
+            palette=cmap_bold,
+            alpha=1.0,
+            edgecolor="black",
+        )
+        plt.title(
+            "3-Class classification (k = %i, weights = '%s')" % (n_neighbors, weights)
+        )
+    plt.show()
+exit(0)
 IrisDict = load_iris()
 
 print(IrisDict.keys())
