6a9951f624ee9fee049a7463dd1e636b989462bc
chuanchuan <dongyuchuan001@outlook.com>
2023-10-01 14:32:14 +0200

    5

--- assignment5/task.py
+++ assignment5/task.py
@@ -1,33 +1,46 @@
 import numpy as np
+action_dic = {0:'N', 1:'S', 2:'W', 3:'E'}
 def BellmanEquation(epsilon: float, V_init: np.array, reward: np.array, transition_prob: np.array, gamma: float):
     V_current = V_init.copy()
     while True:
         V_update = np.zeros((3,3))
         for i in range(np.shape(V_init)[0]):
             for j in range(np.shape(V_init)[1]):
-                V_old = V_current[i][j]
-                # print(V_old)
-                # print(V_current)
-                V_update[i][j] = max(
-                    (transition_prob[0] * (reward[next_s[0]][next_s[1]] + gamma * V_current[next_s[0]][next_s[1]])) + 
-                        transition_prob[1] * (reward[i][j] + gamma * V_old)
-                                      for next_s in [[i-1,j], [i+1,j], [i,j-1], [i,j+1]]
-                                      if 0 <= next_s[0] < np.shape(V_init)[0] and 0 <= next_s[1] < np.shape(V_init)[1])
-                # print("vupdate:", V_update[i][j])
+                value_max = -1
+                value = -1
+                for next_s in [[i-1,j], [i+1,j], [i,j-1], [i,j+1]]:  #N, S, W, E
+                    if 0 <= next_s[0] < np.shape(V_init)[0] and 0 <= next_s[1] < np.shape(V_init)[1]: 
+                        value = ( (transition_prob[0] * (reward[next_s[0]][next_s[1]] + gamma * V_current[next_s[0]][next_s[1]])) +
+                                                transition_prob[1] * (reward[i][j] + gamma * V_current[i][j]))
+                    if value > value_max:
+                        value_max = value
+                V_update[i][j] = value_max           
+        
         if np.max(abs(V_update - V_current)) < epsilon:
-            return V_update
+            policy = np.empty((3,3), dtype=str)
+            for i in range(np.shape(V_init)[0]):
+                for j in range(np.shape(V_init)[1]):
+                    value_max = -1
+                    value = -1
+                    for action_index, next_s in enumerate([[i-1,j], [i+1,j], [i,j-1], [i,j+1]]):
+                        if 0 <= next_s[0] < np.shape(V_init)[0] and 0 <= next_s[1] < np.shape(V_init)[1]: 
+                            value = ( (transition_prob[0] * (reward[next_s[0]][next_s[1]] + gamma * V_current[next_s[0]][next_s[1]])) +
+                                                    transition_prob[1] * (reward[i][j] + gamma * V_current[i][j]))
+                        if value > value_max:
+                            value_max = value
+                            policy[i][j] = action_dic[action_index]
+                        
+            return V_update, policy
         else:
             V_current = V_update.copy()
-            print(V_current)
-
-    
-
-
-
-
+            # print(V_current)
 
 epsilon = 0.1
 reward = np.array([[0,0,0],[0,10,0],[0,0,0]])
-V_init = np.array([[0,0,0],[0,0,0],[0,0,0]])
+V_init = np.array([[0,1,1],[1,0,1],[1,3,0]])
 transition_prob = np.array([0.8,0.2])
-V = BellmanEquation(epsilon, V_init, reward, transition_prob, gamma=0.9)
+V, P = BellmanEquation(epsilon, V_init, reward, transition_prob, gamma=0.9)
+print('Optimal Value:')
+print(V)
+print('Policy:')
+print(P)
